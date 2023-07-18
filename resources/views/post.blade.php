eb189193495203c776f7dbefe7ec64249b7f58d4
hebimonhp96 <quocviet0605@gmail.com>
2023-07-18 17:07:45 +0700

    add files

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+<body>
+    <article>
+      <h1><?= $post->title; ?></h1>
+    </article>
+    <div>
+      <?= $post->body; ?>
+    </div>
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+<body>
+    <?php foreach ($posts as $post):  ?>
+    <article>
+        <h1>
+            <a href="/posts/<?= $post->slug;?>">
+                <?= $post->title ?>
+            </a>
+        </h1>
+        <div><?= $post->body ?></div>
+    </article>
+    <?php endforeach; ?>
+</body>
